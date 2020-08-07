c4f1e2d4f2550890f7f7114ef85b83578a361bb6
RahmanHannan <61734966+RahmanHannan@users.noreply.github.com>
2020-08-07 22:54:16 +0800

    Update Prayer Times.js

--- scripts/Prayer Times.js
+++ scripts/Prayer Times.js
@@ -26,3 +26,39 @@ function mouseLeave() {
 }
 
 window.alert("The prayer times shown will be for a whole month.");
+
+function enquirySubmit()
+{
+  // alert("clicked");
+
+  var name = document.getElementById("fname").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+if(name)
+  {
+    if(email)
+      {
+        if(validateemail(email))
+          alert("Your Name : "+name +"\n Your Email : "+email);
+      }
+      else{
+        alert("Please Enter Email");
+      }
+  }
+  else{
+    alert("Please Enter Name");
+  }
+}
+
+function validateemail(email)
+{
+var x=email;
+var atposition=x.indexOf("@");
+var dotposition=x.lastIndexOf(".");
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+  alert("Please enter a valid e-mail address");
+  return false;
+  }
+  else
+    return true;
+}
