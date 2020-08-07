c3a19a36a5cc2f0eac82e9614a2fea33a5ff567c
rahmaatika96 <61734895+rahmaatika96@users.noreply.github.com>
2020-08-07 20:33:07 +0700

    Update rah-script.js

--- scripts/rah-script.js
+++ scripts/rah-script.js
@@ -16,3 +16,44 @@ function getSourceMasjidJamek() {
 function getSourceIAMM() {
   alert("The source is http://www.kuala-lumpur.ws/magazine/museum-of-islamic-arts.htm");
 }
+function contactSubmit()
+{
+  // alert("clicked");
+
+  var name = document.getElementById("fname").value;
+  var email = document.getElementById("email").value;
+  var num = document.getElementById("mobile_number").value;
+  var subject = document.getElementById("subject").value;
+if(name)
+  {
+    if(email)
+      {
+        if(validateemail(email))
+          {
+            if(validateNum(num))
+            {
+          alert("Your Name : "+name +"\n Your Email : "+email);
+            }
+          }
+      }
+      else{
+        alert("Please Enter Email");
+      }
+  }
+  else{
+    alert("Please Enter Name");
+  }
+}
+
+function validateemail(email)
+{
+var x=email;
+var atposition=x.indexOf("@");
+var dotposition=x.lastIndexOf(".");
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+  alert("Please enter a valid e-mail address");
+  return false;
+  }
+  else
+    return true;
+}
