aa637a2c45cfbb85cfb6e1ec2271c4f5ba66cbf7
rahmaatika96 <61734895+rahmaatika96@users.noreply.github.com>
2020-08-07 16:52:35 +0700

    Add files via upload

---
+++ scripts/rah-script.js
@@ -0,0 +1,18 @@
+function getSourceSriNirwana() {
+  alert("The source is http://www.kuala-lumpur.ws/food-dining/nirwana-maju.htm");
+}
+function getSourceArabesque() {
+  alert("The source is http://www.kuala-lumpur.ws/magazine/arabesque-kuala-lumpur.htm");
+}
+function getSourceMelurThyme() {
+  alert("The source is http://www.kuala-lumpur.ws/magazine/melur-thyme.htm");
+}
+function getSourceNationalMosque() {
+  alert("The source is http://www.kuala-lumpur.ws/attractions/masjid-negara.htm");
+}
+function getSourceMasjidJamek() {
+  alert("The source is http://www.kuala-lumpur.ws/magazine/masjid-jamek.htm");
+}
+function getSourceIAMM() {
+  alert("The source is http://www.kuala-lumpur.ws/magazine/museum-of-islamic-arts.htm");
+}
